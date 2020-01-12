3ea6e46e1d2902b2682898b4de884e39000e072c
gary <garyyaoo@gmail.com>
2020-01-11 16:16:16 -0800

    uwu

--- src/App.js
+++ src/App.js
@@ -34,20 +34,9 @@ function App() {
           <li>
             <Link to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul> */}
+        */}
 
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route path="/" component={Home}/>
         </Switch>
