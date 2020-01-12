0f6d9533fe068e04c181851f52d8d597c5508561
roychen7 <roychen0724@hotmail.com>
2020-01-11 20:02:42 -0800

    added bars

--- src/pages/SessionStatistics.js
+++ src/pages/SessionStatistics.js
@@ -3,12 +3,37 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import {
-    PieChart, Pie, Cell, Tooltip,
+    PieChart, Pie, Cell, Tooltip, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend,
+
   } from 'recharts';
 
   const data = [
     { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
     { name: 'Group C', value: 300 }
+  ];
+
+  const databar = [
+    {
+      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
   ];
 
   const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
@@ -49,7 +74,22 @@ class SessionStatistics extends React.Component {
                     </PieChart>
                 </Grid>
                 <Grid xs={6}>
-                    <h1> Hello world </h1>
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={databar}
+                    margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="pv" fill="#8884d8" />
+                    <Bar dataKey="uv" fill="#82ca9d" />
+                </BarChart>   
                 </Grid>
             </Grid>
             </div>
